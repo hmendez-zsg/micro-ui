040106f0c9fffbeb4dd7a987e37ae768b7593495
hector mendez <hmendez@zobristinc.com>
2021-03-17 14:20:04 -0700

    Implement insults component

--- app/index.tsx
+++ app/index.tsx
@@ -1,5 +1,7 @@
-import React from 'react'
+import "core-js/stable";
+import React from "react";
 import { render } from "react-dom";
+import "regenerator-runtime/runtime";
 import App from "./src/App";
 
 const root = document.getElementById("app-root");
--- app/src/components/Insults.tsx
+++ app/src/components/Insults.tsx
@@ -1,6 +1,26 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { IResponse } from "./types";
 
 const Insults = () => {
+  const [insult, setInsult] = useState("");
+
+  const fetchInsult = async () => {
+    const response = await axios.get<IResponse>(
+      "http://localhost:8080/v2/api/simple/insult"
+    );
+    if (response && response.data && response.data.insult) {
+      setInsult(response.data.insult);
+      console.log(response.data.insult);
+    }
+  };
+
+  useEffect(() => {
+    if (insult === "") {
+      fetchInsult();
+    }
+  }, [insult]);
+
   return (
     <div className="insults">
       <div className="insults__content">
@@ -9,10 +29,15 @@ const Insults = () => {
           src="./public/images/william.png"
           alt="William"
         />
-        <p className="insults__text">
-          <span className="text__speaker">William: </span>Thou art a John-Apple!
-        </p>
-        <button className="insults__cta">Try Again</button>
+        <If condition={insult && insult.length > 0}>
+          <p className="insults__text">
+            <span className="text__speaker">William: </span>
+            {`${insult}!`}
+          </p>
+          <button className="insults__cta" onClick={fetchInsult}>
+            Try Again
+          </button>
+        </If>
       </div>
     </div>
   );
